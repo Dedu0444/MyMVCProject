1412793f9f2d4717440e2cc49afc7c1b496ea80e
Amy Burayu <dedu0444@gmail.com>
2017-10-02 15:20:02 -0400

    Update to Login and SignOut pages

--- DeduMVCProject/DeduMVCProject/Controllers/AccountController.cs
+++ DeduMVCProject/DeduMVCProject/Controllers/AccountController.cs
@@ -68,7 +68,7 @@ namespace DeduMVCProject.Controllers
         public ActionResult SignOut()
         {
             FormsAuthentication.SignOut();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("LogIn", "Account");
         }
 
     }
