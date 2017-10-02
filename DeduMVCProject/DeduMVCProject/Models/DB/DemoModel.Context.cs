309f4950e00c7397e62de024a7255e850533a48f
Amy Burayu <dedu0444@gmail.com>
2017-10-02 10:51:01 -0400

    Building Web Application using Entity Framework and MVC 5 - Tutorial Complete

--- DeduMVCProject/DeduMVCProject/Models/DB/DemoModel.Context.cs
+++ DeduMVCProject/DeduMVCProject/Models/DB/DemoModel.Context.cs
@@ -29,5 +29,7 @@ namespace DeduMVCProject.Models.DB
         public virtual DbSet<SYSUser> SYSUsers { get; set; }
         public virtual DbSet<SYSUserProfile> SYSUserProfiles { get; set; }
         public virtual DbSet<SYSUserRole> SYSUserRoles { get; set; }
+
+        public System.Data.Entity.DbSet<DeduMVCProject.Models.ViewModel.UserProfileView> UserProfileViews { get; set; }
     }
 }
